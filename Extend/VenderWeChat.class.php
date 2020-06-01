f21fef1915832275855247f3d6b1e7eb9a5ac8be
langshen2008 <langshen2008@qq.com>
2020-06-01 10:28:24 +0800

    Update VenderWeChat.class.php

--- Extend/VenderWeChat.class.php
+++ Extend/VenderWeChat.class.php
@@ -11,7 +11,7 @@ class VenderWeChat{
      * @return \Spartan\Extend\WeChatSdk\WeChat
      */
     public function getWeChat($_arrConfig = []){
-        $arrConfig = C('MP_CONFIG');
+        $arrConfig = config('get.MP_CONFIG');
         $arrConfig = array_merge($arrConfig,$_arrConfig);
         if (!isset($arrConfig['APP_ID'])||!isset($arrConfig['APP_SECRET'])||!isset($arrConfig['TOKEN'])){
             \Spt::halt('微信公众号配置不正确。');
